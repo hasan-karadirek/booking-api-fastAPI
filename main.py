f14915a539b7b5e23f69019c6a92aa9ccf8d6f03
unknown <hasankaradirek3@gmail.com>
2024-09-19 21:11:18 +0200

    get user by id endpoint created

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from fastapi import FastAPI,Depends
-from schemas.user import UserIn,UserOut
-from db.database import engine,Base,get_db
-from sqlalchemy.orm import Session
-from crud import user
+from fastapi import FastAPI
+from db.database import engine,Base
 from auth import authentication
-from auth.oauth2 import get_current_user
+from router import user
 
 app=FastAPI()
 
 app.include_router(authentication.router)
+app.include_router(user.router)
 
-@app.post("/create_user", response_model=UserOut)
-def create_user(request:UserIn, db : Session = Depends(get_db), current_user : UserIn = Depends(get_current_user)):
-    return user.create_user(db,request)
 Base.metadata.create_all(engine)
